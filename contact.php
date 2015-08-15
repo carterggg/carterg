51370c5eed15e1b8dc31f9f5cacdc1a9ef0120e1
Carter G <carterguthrie94@yahoo.com>
2015-08-15 04:41:17 -0400

    updated

--- contact.php
+++ contact.php
@@ -23,90 +23,162 @@
 <link rel="stylesheet" href="stylesheet.css" type="text/css" charset="utf-8" />
 
 </head>
+
 <body>
 <!-- Website content -->
+
+<div class="container-fluid">
 	
-	<div class="container-fluid">
-	
-<!-- Row: Header -->
-	
+<!-- Row: mobile nav -->
 	<div class="row">
-		<div class="hidden-xs col-sm-1"></div>
-		<div class=" col-xs-12 col-sm-10">
-				<a href="index.html" title="carter guthrie graphic and web design"><img src="images/layout/bw-header-cg.gif" alt="carter guthrie" class="img-responsive align-center"></a>
-				<br><br><br>
+		<div class="col-xs-12 hidden-md hidden-lg">
+		   <br>
+			<img src="images/layout/menu-icon2.png" class="toggle" width="55px" align="right" alt="m00n">
+  	  	
+   				<nav class="mobile-nav toggle-info">
+   					<ul> <br><br><br>
+   						<li class="main-nav-style mobile-main-nav"><a href="index.html" title="carterguthrie.com home">HOME</a></li>
+					<br>
+					
+					<li class="main-nav-style mobile-main-nav"><a href="graphic/index.html" title="carterguthrie.com graphic design" target="_blank">GRAPHIC</a></li> 
+									<br>
+					<li class="main-nav-style mobile-main-nav"><a href="web/index.html" title="carterguthrie.com web design" target="_blank">WEB</a></li>
+					
+					<br>
+					<li class="main-nav-style mobile-main-nav"><a href="http://carterguthrie.com/photo-video/index.html" title="carterguthrie.com photo and video">PHOTOVIDEO</a></li>
+					
+					<br>
+					<li class="main-nav-style mobile-main-nav"><a href="boltzonboard.html" title="carterguthrie.com boltz on board">JEWELRY</a></li>
+					
+					<br>
+					<li class="main-nav-style mobile-main-nav"><a href="radio.html" title="carterguthrie.com imminent radio">MUSIC</a></li>
+					
+					<br>
+					<li class="main-nav-style mobile-main-nav"><a href="radio.html" title="carterguthrie.com imminent radio">ETC</a></li>
+					
+					<br>
+					<li class="main-nav-style mobile-main-nav"><a href="aboutme.html" title="carterguthrie.com about">ABOUT</a></li>
+					<br>
+					
+					<li class="main-nav-style mobile-main-nav"><a href="contact.php" title="carterguthrie.com contact">CONTACT</a></li>	
+					<br>
+					<li class="main-nav-style mobile-main-nav"><a href="images/myself/carterguthrie-resume-2k15.pdf" title="carterguthrie.com resume">RESUME</a></li>
+  					
+   					</ul>
+   			</nav>
+   			<br><br>			
 		</div>
-		<div class="hidden-xs col-sm-1"></div>
 	</div>
+
+		  
+	
+			
+
 	
 <!-- Row: Navigation -->
-
-
 	
 	<div class="row">
-		<div class="hidden-xs col-sm-1"></div>
-		<div class="hidden-xs col-sm-10">
 		
-			<nav class="align-center">
+		<div class="hidden-xs hidden-sm col-md-2">
+		
+			<nav class="fixed-nav">
 				<ul>
-					<li class="main-nav-style"><a href="index.html" title="">HOME</a></li>
-					<li class="main-nav-style"><a href="graphic/index.html" title="" target="_blank">GRAPHIC</a></li>
-					<li class="main-nav-style"><a href="web/index.html" title="" target="_blank">WEB</a></li>
-					<li class="main-nav-style"><a href="boltzonboard.html" title="">BOLTZ ON BOARD</a></li>
-					<li class="main-nav-style"><a href="radio.html" title="Imminent Radio">RADIO</a></li>
-					<li class="main-nav-style"><a href="http://carterguthrie.com/photo-video/index.html" title="Photo and Video">PHOTO/VIDEO</a></li>
-					<li class="main-nav-style"><a href="aboutme.html" title="About Carter Guthrie">ABOUT</a></li>
-					<li class="main-nav-style"><a href="contact.php" title="Contact Carter Guthrie">CONTACT</a></li>
+					<li class="main-nav-style"><a href="index.html" title="carterguthrie.com home">HOME</a></li>
+					<br>
+					<li class="main-nav-style"><a href="graphic/index.html" title="carterguthrie.com graphic design" target="_blank">GRAPHIC</a></li> 				<br>
+					<li class="main-nav-style"><a href="web/index.html" title="carterguthrie.com web design" target="_blank">WEB</a></li>
+					<br>
+					<li class="main-nav-style"><a href="http://carterguthrie.com/photo-video/index.html" title="carterguthrie.com photo and video">PHOTOVIDEO</a></li>
+					<br>
+					<li class="main-nav-style"><a href="boltzonboard.html" title="carterguthrie.com boltz on board">JEWELRY</a></li>
+					<br>
+					<li class="main-nav-style"><a href="radio.html" title="carterguthrie.com imminent radio">MUSIC</a></li>
+					<br>
+					<li class="main-nav-style"><a href="radio.html" title="carterguthrie.com imminent radio">ETC</a></li>
+					<br>
+					<li class="main-nav-style"><a href="aboutme.html" title="carterguthrie.com about">ABOUT</a></li>
+					<br>
+					<li class="main-nav-style"><a href="contact.php" title="carterguthrie.com contact">CONTACT</a></li>	
+					<br>
+					<li class="main-nav-style"><a href="images/myself/carterguthrie-resume-2k15.pdf" title="carterguthrie.com resume">RESUME</a></li>
+					<br>
 				</ul>
+						  		  <br>
+					<a href="https://www.facebook.com/carterthedesigner" title="carter guthrie facebook" target="_blank"><img src="images/layout/fb2.png" onmouseover="this.src='images/layout/fb.png'" onmouseout="this.src='images/layout/fb2.png'" alt="Facebook: Carter Guthrie" width="30px" class="img-responsive align-center floated-left"></a>
+			
+					<a href="https://www.linkedin.com/in/carterguthrie" title="carter guthrie linkedin" target="_blank"><img src="images/layout/li2.png" onmouseover="this.src='images/layout/li.png'" onmouseout="this.src='images/layout/li2.png'" alt="Linkedin: Carter Guthrie" width="30px" class="img-responsive align-center floated-left"></a>
+			
+					<a href="http://instagram.com/carterthedesigner" title="carter guthrie instagram" target="_blank"><img src="images/layout/ig2.png" onmouseover="this.src='images/layout/ig.png'" onmouseout="this.src='images/layout/ig2.png'" alt="Instagram: Carter Guthrie" width="30px" class="img-responsive align-center floated-left"></a>
+			
+					<a href="https://soundcloud.com/carterggg" title="carter guthrie soundcloud" target="_blank"><img src="images/layout/sc2.png" onmouseover="this.src='images/layout/sc.png'" onmouseout="this.src='images/layout/sc2.png'" alt="Soundcloud: Carter Guthrie" width="30px" class="img-responsive align-center floated-left"></a>
+			
+					<a href="https://www.youtube.com/channel/UCJtoBe2QsTj8O0aoXnWteOA" title="carter guthrie youtube" target="_blank"><img src="images/layout/ut2.png" onmouseover="this.src='images/layout/ut.png'" onmouseout="this.src='images/layout/ut2.png'" alt="Youtube: Carter Guthrie" width="30px" class="img-responsive align-center floated-left"></a>
 			</nav>
 		<br><br><br>
 		</div>
-		<div class="hidden-xs col-sm-1"></div>
-	</div>	
+						
+<!-- Row: Graphic -->
 	
-<!-- First Row: Form -->
-					<div class="row">
-						<div class="col-sm-1"></div>
-							<div class="col-xs-12 col-sm-10">
-							 <h1 class="align-center">CONTACT ME</h1>
+
+		<div class="col-xs-12 col-sm-12 col-md-9">
+			<!-- Header -->
+					  <br>
+				<a href="index.html" title="Carter Guthrie Graphic and Web Design"><img src="images/layout/bw-header-cg.gif" alt="Carter Guthrie" class="img-responsive align-center"></a>
+				<br class="hidden-xs"><br class="hidden-sm hidden-md hidden-lg"><br>
+	
+
+<!-- Main Content -->
+
+	<div class="row">
+
+		<div class="col-sm-1"></div>
+			<div class="col-xs-12 col-sm-10">
+				<h1 class="align-center main-subpage-text">CONTACT ME</h1>
 		   <br>
 								<?php include('inc/form.php'); ?>
-							</div>
-						<div class="col-sm-1"></div>
-					</div>
-
-<!-- Footer row -->
-	
-	<div class="row">
-		<div class="hidden-xs col-sm-1"></div>
-			<div class="col-xs-12 col-sm-10">
-				<nav class="align-center">
-					<ul>
-						<li class="footer-style footer-size"><a href="index.html" title="carterguthrie graphic and web design">HOME</a></li>
-						<li class="footer-style footer-size"><a href="contact.php" title="carterguthrie graphic and web design">CONTACT</a></li>
-						<li class="footer-style footer-size"><a href="images/myself/carterguthrie-resume-2k15.pdf" title="carterguthrie graphic and web design">RESUME</a></li>
-					</ul>
-				</nav>
-		
-				<h1 class="align-center footer-text footer-style"><a href="http://carterguthrie.com" title="carterguthrie graphic and web design">CARTER GUTHRIE</a></h1>
-				<br>
 			</div>
-		<div class="hidden-xs col-sm-1"></div>
-	</div>	
+		<div class="col-sm-1"></div>
+			
+	</div>
+					
+			
+			
+			
+
+
+
+
 		
-	</div>	
+<!-- footer row
+	
+<div class="row">
+	<div class="hidden-xs col-sm-1"></div>
+		<div class="col-xs-12 col-sm-10">
+		<nav class="align-center">
+			<ul>
+				<li class="footer-style footer-size"><a href="index.html" title="carterguthrie graphic and web design">HOME</a></li>
+				<li class="footer-style footer-size"><a href="contact.php" title="carterguthrie graphic and web design">CONTACT</a></li>
+				<li class="footer-style footer-size"><a href="images/myself/carterguthrie-resume-2k15.pdf" title="carterguthrie graphic and web design">RESUME</a></li>
+			</ul>
+		</nav>
+		
+			<h1 class="align-center footer-text footer-style"><a href="http://carterguthrie.com" title="carterguthrie graphic and web design">CARTER GUTHRIE</a></h1>
+			<br>
+		</div>
+
+</div>
+	
+	-->
+		</div></div>
+
+</div><!-- END OF Container-Fluid -->
 <!-- END OF Website content -->
 
 <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js">
-    </script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script> 
+<script src="js/main.js"></script>
+<script src="js/bootstrap.min.js"></script>
     
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script src="http://blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
-	<script src="js/bootstrap-image-gallery.js"></script>
-	<script src="js/main.js"></script>
-<!-- END OF jQuery --> 
+    
 </body>
 </html>
